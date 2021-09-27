548469323e33d967ebd1befec0bbd91d6019a9a3
Pablogomez5 <pablogomezperez01@gmail.com>
2021-09-27 09:06:57 +0200

    Commit inicial del proyecto

---
+++ src/main/java/es/cic/bootcamp/curso12final/Curso12finalApplication.java
@@ -0,0 +1,13 @@
+package es.cic.bootcamp.curso12final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso12finalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso12finalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/bootcamp/curso12final/Curso12finalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.bootcamp.curso12final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curso12finalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
