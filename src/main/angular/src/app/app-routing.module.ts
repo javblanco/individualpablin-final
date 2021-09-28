c2ed7d81720b35b3b1a9b49ba71d0c635dec1d7b
Pablogomez5 <pablogomezperez01@gmail.com>
2021-09-28 14:49:58 +0200

    comienzo de edicción de vistas

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -5,20 +5,16 @@ import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.co
 import { ProductoMaestroComponent } from './producto-maestro/producto-maestro.component';
 import { TipoProductoDetalleComponent } from './tipo-producto-detalle/tipo-producto-detalle.component';
 import { TipoProductoMaestroComponent } from './tipo-producto-maestro/tipo-producto-maestro.component';
-import { TransferenciaComponent } from './transferencia/transferencia.component';
 
 const routes: Routes = [
   {path: 'index', component: IndexComponent},
   {path: '', redirectTo: 'index', pathMatch: 'full'},
-  {path: 'tipo-producto/listar', component: TipoProductoMaestroComponent},
-  {path: 'tipo-producto', component: TipoProductoDetalleComponent},
-  {path: 'tipo-producto/:id', component: TipoProductoDetalleComponent},
-  {path: 'producto/listar', component: ProductoMaestroComponent},
-  {path: 'producto', component: ProductoDetalleComponent},
-  {path: 'producto/:id', component: ProductoDetalleComponent},
-  {path: 'transferir', component: TransferenciaComponent},
-  {path: 'devolver', component: TransferenciaComponent},
-  {path: 'reponer', component: TransferenciaComponent},
+  {path: 'tipoEstancia/listar', component: TipoProductoMaestroComponent},
+  {path: 'tipoEstancia', component: TipoProductoDetalleComponent},
+  {path: 'tipoEstancia/:id', component: TipoProductoDetalleComponent},
+  {path: 'estancia/listar', component: ProductoMaestroComponent},
+  {path: 'estancia', component: ProductoDetalleComponent},
+  {path: 'estancia/:id', component: ProductoDetalleComponent},
 ];
 
 @NgModule({
